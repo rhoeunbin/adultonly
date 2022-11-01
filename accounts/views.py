c18d3164e99286a7f4bc09aeae3e21d0a4180bff
rhoeunbin <rhoeb21@gmail.com>
2022-11-01 12:04:17 +0900

    feature/login

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,8 @@
+from multiprocessing import AuthenticationError
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
 
 # Create your views here.
 def signup(request):
@@ -16,3 +19,21 @@ def signup(request):
     }
 
     return render(request, 'accounts/signup.html', context)
+
+def login(request):
+    if request.user.is_anonymous:
+        if request.method == 'POST':
+            form = AuthenticationForm(request, data=request.POST)
+
+            if form.is_valid():
+                auth_login(request, form.get_user())
+                return redirect('accounts:main')
+        else:
+            form = AuthenticationForm()
+        
+        context = {
+            'form' : form
+        }
+        return render(request, 'accounts/login.html', context)
+    else:
+        return redirect('accounts:main')
