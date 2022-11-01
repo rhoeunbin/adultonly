a823e80257989a4e56d0d434cad4b29ea854fdcc
rhoeunbin <rhoeb21@gmail.com>
2022-11-01 17:31:19 +0900

    feature/change_password

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,8 @@
 from multiprocessing import AuthenticationError
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm, CustomUserChangeForm
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
+from django.contrib.auth import get_user_model, update_session_auth_hash
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.decorators import login_required
@@ -12,7 +12,7 @@ def main(request):
     return render(request, 'accounts/main.html')
     
 def signup(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
@@ -48,23 +48,45 @@ def logout(request):
     auth_logout(request)
     return redirect('accounts:main')
 
-def detail(request, pk):
+def index(request):
+    users = get_user_model().objects.all()
+    context = {
+        'users': users,
+    }
+    return render(request, 'accounts:index.html', context)
+
+def profile(request, pk):
     user = get_user_model().objects.get(pk=pk)
     context = {
         'user':user
     }
-    return render(request, 'accounts/detail.html', context)
+    return render(request, 'accounts/profile.html', context)
 
 @login_required
 def update(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         form = CustomUserChangeForm(request.POST, instance=request.user)
         if form.is_valid():
             form.save()
-            return redirect('accounts:detail', request.user.pk)
+            return redirect('accounts:profile', request.user.pk)
         else:
             form = CustomUserChangeForm(instance=request.user)
         context = {
             'form': form
         }
     return render(request, 'accounts/update.html', context)
+
+@login_required
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            return redirect('accounts:index')
+    else:
+        form = PasswordChangeForm(request.user)
+    context = {
+        'form':form
+    }
+    return render(request, 'accounts/change_password.html', context)
