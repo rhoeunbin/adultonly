7f815c5ef882f3134d663f0bf84955f51ef83ffc
solightnsalt <soldakm@gmail.com>
2022-11-01 12:52:17 +0900

    feature/articles/CRUD 구현

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,19 @@
+from hashlib import blake2b
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+    title = models.CharField(max_length=80)
+    content = models.TextField()
+    image = models.ImageField(upload_to="image/", blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # user, 좋아요 추가하기
+
+
+# class Comment(models.Model):
+#     article = models.ForeignKey("Article", on_delete=models.CASCADE)
+#     content = models.TextField()
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+#     # 유저, 추천
