e70b23c6b00cd3bf26434a8545a55c75304219e8
choikeunyoung <ought119@gmail.com>
2022-11-02 13:36:29 +0900

    feature/base_css main

--- articles/views.py
+++ articles/views.py
@@ -5,7 +5,6 @@ from .forms import RestaurantForm, CommentForm
 from django.core.paginator import Paginator
 
 # from django.contrib.auth.decorators import login_required
-
 # Create your views here.
 def index(request):
     return render(request, "articles/index.html")
