2f66bcedfe70a18ea0d77455d05a6aa3a7bc321d
wook <ifol1129@gmail.com>
2022-11-07 17:30:02 +0900

    fix setting

--- adultonly/settings.py
+++ adultonly/settings.py
@@ -26,9 +26,9 @@ load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG") == "True"
+DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".herokuapp.com"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- adultonly/urls.py
+++ adultonly/urls.py
@@ -27,4 +27,6 @@ urlpatterns = [
     path("articles/", include("articles.urls")),
     path("communities/", include("communities.urls")),
     path("ratings/", include("star_ratings.urls", namespace="ratings")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
