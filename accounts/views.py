77c3e01ff475308400bb9ed0018c955ae8400f26
solightnsalt <soldakm@gmail.com>
2022-11-07 15:23:00 +0900

    feat/thumnail

--- accounts/views.py
+++ accounts/views.py
@@ -73,10 +73,10 @@ def profile(request, pk):
 @login_required
 def update(request):
     if request.method == "POST":
-        form = CustomUserChangeForm(request.POST, instance=request.user)
+        form = CustomUserChangeForm(request.POST, request.FILES, instance=request.user)
         if form.is_valid():
             form.save()
-            return redirect("accounts:profile")
+            return redirect("accounts:profile", request.user.pk)
     else:
         form = CustomUserChangeForm(instance=request.user)
     context = {
